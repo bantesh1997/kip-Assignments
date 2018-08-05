e3ab48668c74140028c1344e4a9196795d33e845
bantesh1997 <bantesh.sharma@knoldus.in>
2018-08-05 23:52:58 +0530

    MeetingAssigment

---
+++ 06-08-2018-MeetingRoom/src/main/scala/com/knoldus/IncrementTimehalfHr.scala
@@ -0,0 +1,35 @@
+package com.knoldus
+
+class IncrementTimehalfHr {
+
+  def increment(time: String): String = {
+
+    val exit_time = Array('z', 'a', ':', 'c', 'd')
+
+    val stringLength = time.length
+    exit_time(4) = time(stringLength - 1)
+
+    if ((time(stringLength - 2).toInt + 3) >= '6'.toInt) {
+      exit_time(3) = (time(stringLength - 2) + 3 - 6).toChar
+      exit_time(2) = ':'
+      exit_time(1) = (time(stringLength - 4) + 1).toChar
+      exit_time(0) = time(stringLength - 5)
+    }
+    else {
+      for (i <- 0 until stringLength) {
+        if (i == 3)
+          exit_time(i) = (time(i) + 3).toChar
+        else
+          exit_time(i) = time(i)
+      }
+    }
+
+    var y = ""
+    for (i <- exit_time) {
+      y += i
+    }
+    y
+  }
+
+
+}
